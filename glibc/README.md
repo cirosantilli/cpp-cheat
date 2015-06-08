7208ce6b9d32fcb0c53e7997df035ece3bb5ff95
Ciro Santilli <ciro.santilli@gmail.com>
2015-06-08 08:32:46 +0200

    Move in syscall

--- glibc/README.md
+++ glibc/README.md
@@ -3,6 +3,7 @@
 1.  [Introduction](introduction.md)
 1.  Usage
     1.  [main.c](main.c)
+    1.  [syscall.c](syscall.c)
     1.  [alloca.c](alloca.c)
 1.  Internals
     1.  [Makefile](makefile.md)
---
+++ glibc/syscall.c
@@ -0,0 +1,84 @@
+/*
+How to make a system call from C.
+
+# syscall
+
+        man syscall
+
+    Function used to make direct system calls from C.
+
+    Takes the syscall number followed by a variable number of arguments:
+
+        syscall(number, arg1, arg2, ...)
+
+    This function is not a bare system call only on what concerns error handling:
+    the same processing that is done for the POSIX library is done in case of error,
+    with error returns always `-1` and `errno` being set accordingly.
+
+    This function automatically puts RAM variables into registers before calling the system call,
+    and gets the return value from `eax` into RAM.
+
+    It is however an exact system call when there is no error situation. For example,
+    syscall getpriority returns values between 0 and 39, while the POSIX version
+    can always return negative values.
+
+    where the arguments are the arguments of the system call
+
+    Don't use bare system call numbers since those vary between architectures.
+    In this way, if you use a syscall that is available to all archs your code is arch portable.
+
+    Most system calls are defined on most architectures and have names which map
+    to the actual number, depending on the architecture. This is done with the
+    `__NR_XXX` constants in `asm/unistd.h` or SYS_<NAME> in `sys/types`
+
+    TODO return value type is long int?
+
+# __NR_XXX macros
+
+    Are defined in the Linux kernel directly.
+
+# SYS_ macros
+
+    Are defined in terms of `__NR_` for compatibility.
+
+    TODO: what is the difference between `asm/unistd.h __NR_X` and `sys/types SYS_NAME`?
+    which is better in which situtaions?
+
+# _syscall macro
+
+    Deprecated method to do direct system calls. Don't use it.
+
+#TODO
+
+    TODO how to get the system call constants such as PRIO_PROCESS without going into POSIX libs?
+    is this exactly how those are exposed? Or is there a way that does not require POSIX headers?
+*/
+
+#define _GNU_SOURCE
+
+#include <assert.h>
+#include <stdio.h>
+
+#include <unistd.h>  /* syscall */
+#include <sys/syscall.h> /*  __NR_XXX, SYS_XXX */
+
+int main() {
+#ifdef __linux__
+    puts("__linux__");
+
+    /*
+    Basic usage without extra arguments.
+    */
+    {
+        assert(syscall(__NR_getpid)  == getpid());
+        assert(syscall(__NR_getppid) == getppid());
+    }
+
+    /*
+    With arguments
+    */
+    {
+        syscall(__NR_exit, 0);
+    }
+#endif
+}
