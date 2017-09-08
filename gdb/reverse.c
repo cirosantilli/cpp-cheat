41704cae6cb14a04824acd1fcf4621edf795afdb
Ciro Santilli <ciro.santilli@gmail.com>
2017-09-08 14:13:37 +0100

    expand reverse using rr tests

--- gdb/reverse.c
+++ gdb/reverse.c
@@ -1,12 +1,51 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main() {
-    int i, j;
-    i = 0;
-    j = 0;
-    i = 1;
-    j = 1;
-    i = 2;
-    j = 2;
+int f() {
+	int i;
+	i = 0;
+	i = 1;
+	i = 2;
+	return i;
+}
+
+int where_return(int i) {
+    if (i)
+        return 0;
+    else
+        return 1;
+}
+
+int main(void) {
+	int i;
+
+	/* Variable changes. */
+	i = 0;
+	i = 1;
+	i = 2;
+
+	/* Local call. */
+	f();
+
+    /* Where it returns. */
+    /* https://stackoverflow.com/questions/3649468/setting-breakpoint-in-gdb-where-the-function-returns */
+    where_return(0);
+    where_return(1);
+
+	/* printf. Meh. How dare they ship this crap.
+	 * Use rr instead for now.
+	 * - https://stackoverflow.com/questions/40125154/target-record-full-in-gdb-makes-n-command-fail-on-printf-with-process-recor/46113357#46113357
+	 * - https://stackoverflow.com/questions/2528918/gdb-reverse-debugging-fails-with-process-record-does-not-support-instruction-0x/46113472#46113472
+	 * - https://stackoverflow.com/questions/42451492/disable-avx-optimized-functions-in-glibc-ld-hwcap-mask-etc-ld-so-nohwcap-for/44468494#44468494
+	 * - https://stackoverflow.com/questions/43750603/gdb-reverse-debugging-avx2
+	 * */
+	printf("i = %d\n", i);
+
+    /* Is randomness completely removed? */
+	srand(time(NULL));
+	i = rand();
+	printf("rand() = %d\n", i);
+
     return EXIT_SUCCESS;
 }
--- gdb/reverse.gdb
+++ gdb/reverse.gdb
@@ -1,5 +1,7 @@
+
 start
 record
+# TODO same as target record-full ?
 
 continue
 reverse-continue
