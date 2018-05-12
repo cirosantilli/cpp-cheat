30e83159376625c21179ab652d63961f3d8aa2fe
Ciro Santilli <ciro.santilli@gmail.com>
2018-05-12 03:01:39 +0100

    __auto_type, statement expression factorial

--- gcc/typeof.c
+++ gcc/typeof.c
@@ -1,9 +1,11 @@
 /*
 # typeof
 
-    Like C++11 decltype.
+Like C++11 decltype.
 
-    Partially reproductible with C11 `_Generic`.
+Partially reproductible with C11 `_Generic`.
+
+https://stackoverflow.com/questions/6513806/would-it-be-possible-to-add-type-inference-to-the-c-language/31709221#31709221
 */
 
 #include "common.h"
@@ -12,5 +14,10 @@ int main() {
     /* Same as: double j = 0.5; */
     typeof(1 + 0.5) j = 0.5;
     assert(j == 0.5);
+
+    /* Similar to C++ auto. */
+    __auto_type k = 0.5;
+    assert(sizeof(j) == sizeof(k));
+
     return EXIT_SUCCESS;
 }
