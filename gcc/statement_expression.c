69e2ce773fb683bf80a70f2615f2755ec12ee04e
Ciro Santilli <ciro.santilli@gmail.com>
2018-05-12 03:01:39 +0100

    __auto_type, statement expression factorial

--- gcc/statement_expression.c
+++ gcc/statement_expression.c
@@ -1,14 +1,35 @@
 /*
-## Statement expression
+# Statement expression
 
-    https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html
+Only the last expression is "returned".
 
-    Only the last expression is returned.
+Especially useful for macros:
+https://stackoverflow.com/questions/10393844/about-typecheck-in-linux-kernel
+where it serves as a "return value"
+
+https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html
 */
+
+#define FACTORIAL(X) \
+    ({ \
+        unsigned int x = X; \
+        unsigned int result = 1; \
+        while (x > 1) { \
+            result *= x; \
+            x--; \
+        } \
+        result; \
+    })
 
 #include "common.h"
 
 int main() {
-    assert(({int i = 0; int j = i; i++; j = i; j;}));
+    /* Minimal example */
+    int x = ({int i = 0; i++; i + 1;});
+    assert(x == 2);
+
+    /* More meaningful macro example*/
+    assert(FACTORIAL(5) == 120);
+
     return EXIT_SUCCESS;
 }
