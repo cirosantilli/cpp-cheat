a2de98bcd4914e1e564bcf0479edb2dee8e96892
Ciro Santilli <ciro.santilli@gmail.com>
2018-08-21 18:04:43 +0100

    heap

--- cpp/interactive/bst_vs_heap.cpp
+++ cpp/interactive/bst_vs_heap.cpp
@@ -1,40 +1,54 @@
 #include "common.hpp"
 
 int main(int argc, char **argv) {
-    size_t i, n;
-    std::priority_queue<int> heap;
-    std::set<int> bst;
+    typedef uint64_t I;
+    I *randoms;
+    size_t i, j, n, granule;
+    std::priority_queue<I> heap;
+    std::set<I> bst;
+    std::uniform_int_distribution<I> dist;
     std::random_device dev;
     unsigned int seed = dev();
     std::mt19937 prng(seed);
-    std::uniform_int_distribution<> dist;
+
     if (argc > 1) {
         n = std::stoi(argv[1]);
     } else {
         n = 1000000;
     }
+    if (argc > 2) {
+        granule = std::stoi(argv[2]);
+    } else {
+        granule = 10;
+    }
+    randoms = new I[granule];
+    for (i = 0; i < n / granule; ++i) {
+        for (j = 0; j < granule; ++j) {
+            randoms[j] = dist(prng);
+        }
 
-    for (i = 0; i < n; ++i) {
-        auto random_value = dist(prng);
-
-        // BST
-        auto start = std::chrono::steady_clock::now();
-        auto ret = bst.insert(random_value);
-        auto end = std::chrono::steady_clock::now();
+        // BST.
+        auto start = std::chrono::high_resolution_clock::now();
+        for (j = 0; j < granule; ++j) {
+            bst.insert(randoms[j]);
+        }
+        auto end = std::chrono::high_resolution_clock::now();
         auto dt_bst = end - start;
-        // Heaps can have dupes, BST cannot, so skip them for both.
-        if (!ret.second) continue;
 
-        // Heap
-        start = std::chrono::steady_clock::now();
-        heap.push(random_value);
-        end = std::chrono::steady_clock::now();
+        // Heap.
+        start = std::chrono::high_resolution_clock::now();
+        for (j = 0; j < granule; ++j) {
+            heap.emplace(randoms[j]);
+        }
+        end = std::chrono::high_resolution_clock::now();
         auto dt_heap = end - start;
 
-        std::cout << random_value << " "
+        // Output.
+        std::cout
             << std::chrono::duration_cast<std::chrono::nanoseconds>(dt_heap).count() << " "
             << std::chrono::duration_cast<std::chrono::nanoseconds>(dt_bst).count() << std::endl;
     }
+    delete[] randoms;
 
     // Sanity check.
     for (auto it = bst.rbegin(); it != bst.rend(); ++it) {
