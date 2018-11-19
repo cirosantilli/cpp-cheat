f8171dd83a0928b6206868e2f7c93c90e3753ad1
Ciro Santilli <ciro.santilli@gmail.com>
2018-11-19 00:00:00 +0000

    cpp string: update comment style

--- cpp/string.cpp
+++ cpp/string.cpp
@@ -2,19 +2,14 @@
 
 #include "common.hpp"
 
-std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        elems.push_back(item);
-    }
-    return elems;
-}
-
 std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, elems);
-    return elems;
+    std::string item;
+    std::stringstream ss(s);
+    std::vector<std::string> output;
+    while (std::getline(ss, item, delim)) {
+        output.push_back(item);
+    }
+    return output;
 }
 
 int main() {
@@ -31,17 +26,16 @@ int main() {
         assert(oss.str() == "abc");
     }
 
-    /* # + for strings
-     *
-     * # cat
-     *
-     * # concatenate.
-     *
-     * Creates a new string.
-     *
-     * The only way to do inline this without creating a new string seems to be by using stringstream.
-     * http://stackoverflow.com/questions/662918/how-do-i-concatenate-multiple-c-strings-on-one-line
-     */
+    // # + for strings
+    //
+    // # cat
+    //
+    // # concatenate.
+    //
+    // Creates a new string.
+    //
+    // The only way to do inline this without creating a new string seems to be by using stringstream.
+    // http://stackoverflow.com/questions/662918/how-do-i-concatenate-multiple-c-strings-on-one-line
     {
         std::string s = "ab";
         std::string s1 = "cd";
@@ -65,12 +59,11 @@ int main() {
         //s[3] = 'd';
     }
 
-    /* # lowercase
-     *
-     * http://stackoverflow.com/questions/313970/stl-string-to-lower-case
-     *
-     * Boost has a single function: boost::algorithm::to_lower(str);
-     */
+    // # lowercase
+    //
+    // http://stackoverflow.com/questions/313970/stl-string-to-lower-case
+    //
+    // Boost has a single function: boost::algorithm::to_lower(str);
     {
         // Best stdlib way with transform:
         std::string s = "AbCd1_";
@@ -78,10 +71,9 @@ int main() {
         assert(s == "abcd1_");
     }
 
-    /* # c_str
-     *
-     *  Convert std::string to C null terminated char* string.
-     */
+    // # c_str
+    //
+    //  Convert std::string to C null terminated char* string.
     {
         std::string s = "abc";
         assert((std::strcmp(s.c_str(), "abc")) == 0);
@@ -95,30 +87,26 @@ int main() {
 
     // # Split at a character into array of strings.
     {
-        // Best stdlib solution for any character: http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c
+        // Best stdlib solution for any character:
+        // http://stackoverflow.com/questions/236129/how-to-split-a-string-in-c
         // There are shorters sstream solutions that split at whitespace.
         // For Boost it's a one liner.
         {
             assert((split("01:23::45", ':') == std::vector<std::string>{"01", "23", "", "45"}));
-
-            std::vector<std::string> v;
-            split("01:23::45", ':', v);
-            assert((v == std::vector<std::string>{"01", "23", "", "45"}));
         }
     }
 
-    /* # strip
-     *
-     * # chomp
-     *
-     * # trim
-     *
-     * Exact same techniques as removing elements from vectors but for characters.
-     *
-     * It's just that those operations are so common on strings...
-     *
-     * http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
-     */
+    // # strip
+    //
+    // # chomp
+    //
+    // # trim
+    //
+    // Exact same techniques as removing elements from vectors but for characters.
+    //
+    // It's just that those operations are so common on strings...
+    //
+    // http://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
     {
         // A single character: remove and erase idiom.
         // Single remove_all call in Boost.
@@ -142,19 +130,18 @@ int main() {
         }
     }
 
-    /* # getline
-     *
-     * Read istream until a any given character, by default newline, and store chars read into a string.
-     *
-     * The other major method of getting data from streams is `operator<<`,
-     * which generaly speaking reads until whitespace. getline is generaly saner.
-     *
-     * Returns the stream itself, which allows to:
-     *
-     * - chain calls
-     * - do while(getline) combos, as streams can be converted to bool via the `void*()`
-     *   operator which returns a pointer type which is then converted to a boolean.
-     */
+    // # getline
+    //
+    // Read istream until a any given character, by default newline, and store chars read into a string.
+    //
+    // The other major method of getting data from streams is `operator<<`,
+    // which generaly speaking reads until whitespace. getline is generaly saner.
+    //
+    // Returns the stream itself, which allows to:
+    //
+    // - chain calls
+    // - do while(getline) combos, as streams can be converted to bool via the `void*()`
+    //   operator which returns a pointer type which is then converted to a boolean.
     {
         // Up to newline.
         {
@@ -219,35 +206,31 @@ int main() {
         assert(oss.str() == "abcd");
     }
 
-    /* # int to string
-     *
-     * There are a few standard alternatives.
-     *
-     * http://stackoverflow.com/questions/5590381/easiest-way-to-convert-int-to-string-in-c
-     */
+    // # int to string
+    //
+    // There are a few standard alternatives.
+    //
+    // http://stackoverflow.com/questions/5590381/easiest-way-to-convert-int-to-string-in-c
     {
-        /* C++11 solves the question once and for all with a robust one-liner for base types.
-         *
-         * It is not intended however for class input.
-         */
+        // C++11 solves the question once and for all with a robust one-liner for base types.
+        //
+        // It is not intended however for class input.
 #if __cplusplus >= 201103L
         assert(std::to_string(123) == "123");
 #endif
 
-        /* std::stringstream seems to be the best pre C++11 solution.
-         *
-         * It also has the advantage of working for any class that implements `operator<<`.
-         */
+        // std::stringstream seems to be the best pre C++11 solution.
+        //
+        // It also has the advantage of working for any class that implements `operator<<`.
         {
             std::stringstream oss;
             oss << 123;
             assert(oss.str() == "123");
         }
 
-        /* C sprintf.
-         *
-         * Works, but uses too many conversion operations.
-         */
+        // C sprintf.
+        //
+        // Works, but uses too many conversion operations.
         {
             char cs[16];
             std::sprintf(cs, "%d", 123);
@@ -256,10 +239,9 @@ int main() {
         }
     }
 
-    /* # string to int
-     *
-     * http://stackoverflow.com/questions/7663709/convert-string-to-int-c
-     */
+    // # string to int
+    //
+    // http://stackoverflow.com/questions/7663709/convert-string-to-int-c
     {
         // Best C++11 error checking option: stoi
 #if __cplusplus >= 201103L
