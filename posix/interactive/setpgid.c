0c430ccc0594bc540260992068e2437b8a8dcc4d
Ciro Santilli <ciro.santilli@gmail.com>
2018-12-13 10:06:57 +0000

    setpgid: print parent / child on signal handler

--- posix/interactive/setpgid.c
+++ posix/interactive/setpgid.c
@@ -1,70 +1,99 @@
 /* # setpgid
  *
- * http://pubs.opengroup.org/onlinepubs/9699919799/functions/setpgid.html
- *
- * Without argument, we change the process group of the child with setpgid:
- *
- *     ./setpgid.out
- *
- * and the program first outputs immediately:
- *
- *     child pid, pgid = 16396, 16396
- *     parent pid, pgid = 16395, 16395
- *     sigint
- *
- * and then whenever you hit Ctrl + C, it prints:
- *
- *     sigint
- *
- * once.
- *
- * With an argument:
- *
- *     ./setpgid.out 0
- *
- * The initial output shows that both child and parent have the same pgid as in:
- *
- *     child pid, pgid = 18747, 18746
- *     parent pid, pgid = 18746, 18746
- *
- * and then each Ctrl + C outputs sigint twice:
- *
- *     sigint
- *     sigint
- *
- * See also:
- *
  * - https://unix.stackexchange.com/questions/149741/why-is-sigint-not-propagated-to-child-process-when-sent-to-its-parent-process/465112#465112
  * - https://stackoverflow.com/questions/31907212/will-ctrlc-send-sigint-signals-to-both-parent-and-child-processes-in-linux/52042820#52042820
  * - https://stackoverflow.com/questions/6108953/how-does-ctrl-c-terminate-a-child-process/52042970#52042970
  *
- * # Process group
+ * **Run without `setpgid`**
  *
- * http://en.wikipedia.org/wiki/Process_group
+ * Without any CLI arguments, `setpgid` is not done:
  *
- * # getpgrp
+ *     ./setpgid
  *
- * Same at `getpgid(0)`.
+ * Possible outcome:
  *
- * # setpgrp
+ *     child pid, pgid = 28250, 28249
+ *     parent pid, pgid = 28249, 28249
+ *     sigint parent
+ *     sigint child
  *
- * Same at `setpgid(0, 0)`, deprecated in POSIX 7.
+ * and the program hangs.
+ *
+ * As we can see, the pgid of both processes is the same, as it gets inherited across `fork`.
+ *
+ * Then whenever you hit:
+ *
+ *     Ctrl + C
+ *
+ * It outputs again:
+ *
+ *     sigint parent
+ *     sigint child
+ *
+ * This shows how:
+ *
+ * - to send a signal to an entire process group with `kill(-pgid, SIGINT)`
+ * - Ctrl + C on the terminal sends a kill to the entire process group by default
+ *
+ * Quit the program by sending a different signal to both processes, e.g. SIGQUIT with `Ctrl + \`.
+ *
+ * **Run with `setpgid`**
+ *
+ * If you run with an argument, e.g.:
+ *
+ *     ./setpgid 1
+ *
+ * then the child changes its pgid, and now only a single sigint gets printed every time from the parent only:
+ *
+ *     child pid, pgid = 16470, 16470
+ *     parent pid, pgid = 16469, 16469
+ *     sigint parent
+ *
+ * You can still kill the parent as before with a SIGQUIT:
+ *
+ *     Ctrl + \
+ *
+ * however the child now has a different PGID, and does not receive that signal! This can seen from:
+ *
+ *     ps aux | grep setpgid
+ *
+ * You will have to kill it explicitly with:
+ *
+ *     kill -9 16470
+ *
+ * This makes it clear why signal groups exist: otherwise we would get a bunch of processes left over to be cleaned manually all the time.
+ *
+ * Tested on Ubuntu 18.04.
  */
 
-#include "common.h"
+#define _XOPEN_SOURCE 700
+#include <assert.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+volatile sig_atomic_t is_child = 0;
 
 void signal_handler(int sig) {
-    char sigint_str[] = "sigint\n";
+    char parent_str[] = "sigint parent\n";
+    char child_str[] = "sigint child\n";
     signal(sig, signal_handler);
     if (sig == SIGINT) {
-        write(STDOUT_FILENO, sigint_str, sizeof(sigint_str) - 1);
+        if (is_child) {
+            write(STDOUT_FILENO, child_str, sizeof(child_str) - 1);
+        } else {
+            write(STDOUT_FILENO, parent_str, sizeof(parent_str) - 1);
+        }
     }
 }
 
 int main(int argc, char **argv) {
-    COMMON_UNUSED(argv);
     pid_t pid, pgid;
 
+    (void)argv;
     signal(SIGINT, signal_handler);
     signal(SIGUSR1, signal_handler);
     pid = fork();
@@ -76,7 +105,8 @@ int main(int argc, char **argv) {
          * > the child has its own unique process ID, and this PID does not match
          * > the ID of any existing process group (setpgid(2)) or session.
          */
-        if (argc == 1) {
+        is_child = 1;
+        if (argc > 1) {
             setpgid(0, 0);
         }
         printf("child pid, pgid = %ju, %ju\n", (uintmax_t)getpid(), (uintmax_t)getpgid(0));
